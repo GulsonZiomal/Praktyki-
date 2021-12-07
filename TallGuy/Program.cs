2496adf00815dab9f1adafd1399e9d293965a4c0
GulsonZiomal <wegneraleksander95@gmail.com>
2021-12-07 09:41:29 +0100

    Update

---
+++ TallGuy/Program.cs
@@ -0,0 +1,14 @@
+﻿public class TallGuy{
+    public string Name = "";
+    public int Height = 0;
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine("Nazwyam się " + Name + " i mam " + Height + "cm wzrostu.");
+    }
+
+    static void Main(string[] args)
+    {
+        TallGuy tallguy = new TallGuy() { Height = 174, Name = "Adam"};
+        tallguy.TalkAboutYourself();
+    }
+}
